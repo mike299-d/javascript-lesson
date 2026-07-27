3f7cfcbf29e6e3614685a8078683c1b32220cca4
mike299-d <mike.ademolaju@gmail.com>
2026-07-27 13:48:24 +0100

    REMOVE DEFAULT TEST AND ADD MONEY TEST

---
+++ jasmine-standalone-5.1.1/moneytest.js
@@ -0,0 +1,16 @@
+import {FormatCurrency} from "../scripts/utilis/money.js"
+
+describe("test suite:FormatCurrency",()=>{
+    it("convert cents into dollars",()=>{
+        expect(FormatCurrency(2095)).toEqual("20.95");
+    });
+    it("works with zero",()=>{
+        expect(FormatCurrency(0)).toEqual("0.00");
+    })
+    it("rounds up to the nearest ",()=>{
+        expect(FormatCurrency(2000.5)).toEqual("20.01")
+    })
+    it("rounds down to the nearest ",()=>{
+        expect(FormatCurrency(2000.4)).toEqual("20.00")
+    })
+});
