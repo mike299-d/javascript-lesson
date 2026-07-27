1f57ed73c37998404d3fe99c0da9ad489190c9eb
mike299-d <mike.ademolaju@gmail.com>
2026-07-27 11:29:03 +0100

    created a test suite for formatCurrency

--- test/moneytest.js
+++ test/moneytest.js
@@ -1,6 +1,30 @@
-import {formatCurrency} from "../scripts/utilis/money.js"
-if (formatCurrency(2095)==="20.95"){
+import {FormatCurrency} from "../scripts/utilis/money.js"
+console.log("test suite : Formatcurrency")
+console.log('convert cents into dollars')
+if (FormatCurrency(2095)==="20.95"){
 console.log("passed");
 }else{
     console.log("failed");
+}
+console.log("Works with zero")
+if(FormatCurrency(0)=== "0.00"){
+    console.log("passed");
+}else{
+    console.log("failed")
+}
+console.log("rounds up to the nearest cents")
+//situation=test-case
+// two types of test cases basic test cases and edge cases
+// basic test cases tests if the code is working 
+// edge  cases test with values that are tricky
+if (FormatCurrency(2000.5)==="20.01"){
+    console.log("passed")
+}else{
+    console.log("failed")
+}
+console.log("rounds up to the nearest cents")
+if (FormatCurrency(2000.4)==="20.00"){
+    console.log("passed")
+}else{
+    console.log("failed")
 }
