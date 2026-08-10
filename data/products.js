858e707a1f62ddf77584c7b9e0a1673722ee962e
mike299-d <mike.ademolaju@gmail.com>
2026-08-10 10:53:39 +0100

    USE INHERITANCE AND POLYMORPHISM SIZE CHART

--- data/products.js
+++ data/products.js
@@ -32,7 +32,26 @@ class Product{
  getPrice(){
    return`$${FormatCurrency(this.priceCents)}`;
  }
+ extraInfoHTML(){
+   return "";
+
+ }
+} 
+class Clothing extends Product{
+ sizeChartLink;
+ constructor(productDetails){
+  super(productDetails);
+  this.sizeChartLink=productDetails.sizeChartLink;
+  
+ }
+ extraInfoHTML(){
+  // super.extraInfoHTML();
+  return `
+    <a href="${this.sizeChartLink}" target="_blank">Size chart</a>
+  `
+ }
 }
+
 export const products = [
   {
     id: "e43638ce-6aa0-4b85-b27f-e1d07eb678c6",
@@ -721,6 +740,11 @@ export const products = [
     ]
   }
 ].map((productDetails)=>{
+  if(productDetails.type=== "clothing"){
+    return new Clothing(productDetails);
+
+
+  }
   return new Product(productDetails);
 });
 console.log(products)
