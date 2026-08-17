c5a099ca5509473e8df78e6f5ce0db82447fd30d
mike299-d <mike.ademolaju@gmail.com>
2026-08-17 13:14:14 +0100

    cretaed the load

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -1,7 +1,10 @@
 import {renderOptionSummary} from './checkout/ordersummary.js';
 import {renderPaymentSummary} from './checkout/paymentSummary.js';
+import { loadProducts } from '../data/products.js';
 // import"../data/cart-class.js" ;
-import'../data/Backend-practice.js';
-
-renderOptionSummary();
-renderPaymentSummary();
+// import'../data/Backend-practice.js';
+loadProducts(()=>{
+    renderOptionSummary();
+    renderPaymentSummary();
+}); 
+ 
