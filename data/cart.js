5e92eeed65c213ff630ddb1f950c34ba6783f861
mike299-d <mike.ademolaju@gmail.com>
2026-08-21 14:23:15 +0100

    added a function to practice loading cart from backend converted code from callback to promises

--- data/cart.js
+++ data/cart.js
@@ -93,3 +93,16 @@ export function updateQuantity(productId, newQuantity) {
   saveStorage();
  }
  
+
+  export  function loadCart(fun){
+   const xhr= new XMLHttpRequest()
+   xhr.addEventListener("load",()=>{
+ 
+    console.log(xhr.response)
+
+ 
+  fun(); 
+   })
+   xhr.open("GET","https://supersimplebackend.dev/cart")
+   xhr.send();
+  }
--- scripts/checkout.js
+++ scripts/checkout.js
@@ -3,21 +3,50 @@ import { renderPaymentSummary } from "./checkout/paymentSummary.js";
 import { loadProducts } from "../data/products.js";
 // import"../data/cart-class.js" ;
 // import'../data/Backend-practice.js';
+import { loadCart } from "../data/cart.js";
 
+Promise.all([
+  new Promise((resolve) => {
+   
+  loadProducts(()=>{
+    resolve("value1");
+  });
+}),
+ new Promise((resolve)=>{
+     loadCart(()=>{
+      resolve();
+     });
+  })
+]).then((values)=>{
+  console.log(values)
+   renderOptionSummary();
+   renderPaymentSummary(); 
+})
+/*
 new Promise((resolve) => {
    
   loadProducts(()=>{
-    console.log("finished loading")
-    resolve();
+    resolve("value1");
   });
     
+}).then((value)=>{
+  return new Promise((resolve)=>{
+     loadCart(()=>{
+      resolve();
+     });
+  })
+
 }).then(()=>{
-    console.log("next step")
+   renderOptionSummary();
+   renderPaymentSummary();
 })
+*/
 
-
-loadProducts(() => {
-  renderOptionSummary();
-  renderPaymentSummary();
-});
+// loadProducts(() => {
+//   loadCart(()=>{
+//     renderOptionSummary();
+//     renderPaymentSummary();
+//   });
+ 
+// });
 
